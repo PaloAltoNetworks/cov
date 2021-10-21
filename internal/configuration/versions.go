f8f9754d287404d6c67fe773b6c0547ead3f1489
CyrilPeponnet <cyril@peponnet.fr>
2021-10-21 15:14:05 -0700

    Set version

--- internal/configuration/versions.go
+++ internal/configuration/versions.go
@@ -3,7 +3,7 @@ package configuration
 // Various version information.
 var (
 	ProjectName    = "cov"
-	ProjectVersion = ""
-	ProjectSha     = "HEAD"
+	ProjectVersion = "v1.0.0"
+	ProjectSha     = "2d664e859f2279533c542491343b343dbb8edfae"
 	ProjectRelease = "dev"
 )
